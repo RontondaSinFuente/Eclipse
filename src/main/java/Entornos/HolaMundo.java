75c0bce1987c5e5c22428163d908a7c40782ddad
RotondaSinFuente <david.rodgal@educa.jcyl.es>
2023-04-14 20:26:25 +0200

    Mi primir commit

---
+++ src/main/java/Entornos/HolaMundo.java
@@ -0,0 +1,13 @@
+package Entornos;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Holaaaaaaaaa");
+		System.out.println("Hola");
+		System.out.println("Holaa");
+		System.out.println("Holaaa");
+	}
+
+}
